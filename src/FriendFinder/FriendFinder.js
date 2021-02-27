0bf832b3de448d15a240b135e16349ccd1160e2e
Kaan Ersoy <kaanersoy12@gmail.com>
2021-02-27 14:05:40 +0300

    Update the fetch function

--- src/FriendFinder/FriendFinder.js
+++ src/FriendFinder/FriendFinder.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react';
-import Friend from '../Friend';
+import React, { Component } from "react";
+import Friend from "../Friend";
 
-import './FriendFinder.scss';
+import "./FriendFinder.scss";
 
 export class FriendFinder extends Component {
+  fetchPeople = () => {
+    fetch("https://randomuser.me/api/")
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  };
 
-    fetchPeople = () => {
-        fetch('')
-    }
-
-
-    render() {
-        const { friendPhoto } = this.props;
-        return (
-        <div className="friend-finder">
-            <img src={friendPhoto} alt="human" />
-                <Friend name='Gokhan' lastName="Ipek" />
-            <div>
-              kisi bilgileri
-            </div>
-            <button>Find a friend!</button>
-            <button>Add as friend!</button>
-        </div>
-        )
-    }
+  render() {
+    const { friendPhoto } = this.props;
+    return (
+      <div className="friend-finder">
+        <img src={friendPhoto} alt="human" />
+        <Friend name="Gokhan" lastName="Ipek" />
+        <div>kisi bilgileri</div>
+        <button>Find a friend!</button>
+        <button>Add as friend!</button>
+      </div>
+    );
+  }
 }
 
-export default FriendFinder
+export default FriendFinder;
