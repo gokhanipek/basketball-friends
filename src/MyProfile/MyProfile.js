fb8a261321a6a6e4e4d2bedcc6805dc253307514
Gokhan Ipek <goekhan.ipek@tipico.com>
2021-02-28 09:54:10 +0100

    my profile state update

--- src/MyProfile/MyProfile.js
+++ src/MyProfile/MyProfile.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react'
 import './MyProfile.scss';
 
 export class MyProfile extends Component {
+
+    state = {
+        targetValue: ''
+    }
     render() {
+        const focusHandler = (event) => {
+            this.setState({ targetValue: event.target.value })
+        }
         return (
         <div className="my-profile">
             <div>My info</div>
             <div>My friends</div>
+            <input onChange={focusHandler} placeholder="Your Name" />
         </div>
         )
     }
